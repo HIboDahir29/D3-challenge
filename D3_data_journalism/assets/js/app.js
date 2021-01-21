48c5d979f1e33bb5524e6e34c26b52733757c04c
Hibo <lm7024an@go.minnstate.edu>
2021-01-21 16:35:11 -0600

    Code update in js file

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,47 @@
 // @TODO: YOUR CODE HERE!
+// svg container
+
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 80,
+    left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our scatter plot,
+// and shift the latter by left and top margins.
+var svg = d3
+    .select(".scatter")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Import Data
+
+d3.csv("assets/data/data.csv").then(function (censusData) {
+    console.log(`The Data: ${censusData}`);
+
+    // Parse Data
+    censusData.forEach(function (data) {
+        data.healthcare = +data.healthcare;
+        data.poverty = +data.poverty;
+    });
+
+    var xLinearScale = d3.scaleLinear()
+        .domain([d3.min(censusData, d => d.poverty) - 1, d3.max(censusData, d => d.poverty) + 1])
+        .range([0, chartWidth]);
+
+    var yLinearScale = d3.scaleLinear()
+        .domain([d3.min(censusData, d => d.healthcare) - 2, d3.max(censusData, d => d.healthcare) + 2])
+        .range([0, chartWidth]);
+
+});
